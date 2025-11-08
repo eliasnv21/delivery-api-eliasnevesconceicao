cff4250164c3e13decc75ddc905b0405e6ae2532
eliasnv21 <eliasnv21@gmail.com>
2025-11-08 15:45:46 -0300

    Health Controller

---
+++ src/main/java/com/deliverytech/delivery/controller/healthController.java
@@ -0,0 +1,22 @@
+package com.deliverytech.delivery.controller;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class healthController {
+
+    @GetMapping("/health")
+    public Map<String, String> health() {
+        return Map.of(
+            "status", "UP",
+            "timestamp", LocalDateTime.now().toString(),
+            "Service", "Delivery API"
+        );
+    }
+
+}
